3f0d29683b0c697d8010a5a606599c341841ec4f
LucasNarciso28 <lucas.narciso.schultz28@gmail.com>
2025-05-06 08:51:27 -0300

    ..

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,19 @@
 function Menu() {
   return (
-    <img src={logo} alt="logo da empresa" />,
-    <aside>
-      <ul>
-        <li>Início</li>
-        <li>Perfil</li>
-        <li>Mensagem</li>
-        <li>Contato</li>
-      </ul>
-    </aside>,
-    <footer>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, a tempora. Beatae similique voluptatem assumenda illo molestiae itaque officiis unde delectus quam explicabo ex reiciendis vero, ipsum ab distinctio labore.</p>
-    </footer>
+    <div>
+      <img src={logo} alt="logo da empresa" />
+      <aside>
+        <ul>
+          <li>Início</li>
+          <li>Perfil</li>
+          <li>Mensagem</li>
+          <li>Contato</li>
+        </ul>
+      </aside>
+      <footer>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, a tempora. Beatae similique voluptatem assumenda illo molestiae itaque officiis unde delectus quam explicabo ex reiciendis vero, ipsum ab distinctio labore.</p>
+      </footer>
+    </div>
   );
 }
 export default Menu;
