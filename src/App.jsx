4ffb8032d2b941de2b26c3648c3c55158f5ede9f
LucasNarciso28 <lucas.narciso.schultz28@gmail.com>
2025-06-09 09:58:58 -0300

    Sobre nós

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import Menu from "./components/Menu.jsx";
 import Profissionais from "./pages/profissionais.jsx";
-import Filtro from "./components/filtro.jsx"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SobreNos from "./pages/sobreNos.jsx"
 
 
+// <><Menu /><Profissionais /></>
 function App() {
   return (
     <BrowserRouter>
-        <Menu></Menu>
       <Routes>
-            {/* { <Route path="/" element={<HomePage />} /> */}
-            {/* <Route path="/cadastro" element={<CadastroPage />} />Vou começar a ver isso daqui tá */}
-            {/* <Route path="/sobre" element={<SobreNosPage />} /> } */}
-            <Route path="/" element={<><Menu /><Profissionais /></>} />
-            {/* <Route path="/mensagens" element={<MensagensPage />} />
-            <Route path="/contato" element={<ContatoPage />} /> */}
-        </Routes>
+        <Route path="/" element={<><Menu /><Profissionais /></>} />
+        <Route path="/sobre" element={<><Menu /><SobreNos /></>} />
+      </Routes>
     </BrowserRouter>
   );
 }
 export default App;
+
+{/* <Route path="/cadastro" element={<CadastroPage />} />
+<Route path="/perfil" element={<ProfilePage />} />
+<Route path="/mensagens" element={<MensagensPage />} />
+<Route path="/contato" element={<ContatoPage />} /> */}
