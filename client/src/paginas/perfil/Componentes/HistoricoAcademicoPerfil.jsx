d12080593acc82bae61924d636fc8a101cd33aee
Rhuam7282 <rhuamramos7282@gmail.com>
2025-10-13 15:10:05 -0300

    perfil bonitao

--- client/src/paginas/perfil/Componentes/HistoricoAcademicoPerfil.jsx
+++ client/src/paginas/perfil/Componentes/HistoricoAcademicoPerfil.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const HistoricoAcademicoPerfil = ({ historicoAcademico }) => {
   return (
-    <div className="flexItem margemInferiorGrande">
-      <h2 className="bordaInferiorSubtle">Histórico Acadêmico</h2>
-      <div className="gridContainer gridColunasAuto gapPequeno">
+    <div className="secaoHistorico">
+      <h2>Histórico Acadêmico</h2>
+      <div className="listaAcademica">
         {historicoAcademico.length > 0 ? (
           historicoAcademico.map((item, index) => (
-            <div key={index} className="cartaoAcademico">
+            <div key={index} className="destaque2">
               <h3>{item.nome}</h3>
               <p>{item.instituicao}</p>
-              <p className="textoMarromEscuro">{item.periodo}</p>
+              <p className="periodo">{item.periodo}</p>
             </div>
           ))
         ) : (
-          <div className="cartaoAcademico">
+          <div className="destaque2">
             <p>Nenhum histórico acadêmico cadastrado.</p>
           </div>
         )}
